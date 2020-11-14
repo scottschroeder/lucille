9058e97e05e615f31c5798a879a46cc557b6b9ed
Scott Schroeder <scottschroeder@sent.com>
2020-11-14 16:23:07 -0500

    basic search over episodes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,95 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+mod srt_loader;
+mod search;
+
+fn main() -> Result<()> {
+    color_backtrace::install();
+    let args = cli::get_args();
+    setup_logger(args.occurrences_of("verbosity"));
+    log::trace!("Args: {:?}", args);
+
+    match args.subcommand() {
+        ("test", Some(sub_m)) => test_fn(sub_m),
+        ("", _) => Err(anyhow::anyhow!(
+            "Please provide a command:\n{}",
+            args.usage()
+        )),
+        subc => Err(anyhow::anyhow!(
+            "Unknown command: {:?}\n{}",
+            subc,
+            args.usage()
+        )),
+    }
+    .map_err(|e| {
+        log::error!("{:?}", e);
+        anyhow::anyhow!("unrecoverable lucile failure")
+    })
+}
+
+fn test_fn(args: &clap::ArgMatches) -> Result<()> {
+    let subs = srt_loader::parse_adsubs()?;
+    let q = args.value_of("query").unwrap_or("default");
+    let r = search::search(q, &subs);
+    log::info!("{:#?}", r);
+    Ok(())
+}
+
+
+fn setup_logger(level: u64) {
+    let mut builder = pretty_env_logger::formatted_timed_builder();
+
+    let noisy_modules = &[
+        "hyper",
+        "mio",
+        "tokio_core",
+        "tokio_reactor",
+        "tokio_threadpool",
+        "fuse::request",
+        "rusoto_core",
+        "want",
+        "tantivy",
+    ];
+
+    let log_level = match level {
+        //0 => log::Level::Error,
+        0 => log::LevelFilter::Warn,
+        1 => log::LevelFilter::Info,
+        2 => log::LevelFilter::Debug,
+        _ => log::LevelFilter::Trace,
+    };
+
+    if level > 1 && level < 4 {
+        for module in noisy_modules {
+            builder.filter_module(module, log::LevelFilter::Info);
+        }
+    }
+
+    builder.filter_level(log_level);
+    builder.format_timestamp_millis();
+    //builder.format(|buf, record| writeln!(buf, "{}", record.args()));
+    builder.init();
+}
+
+mod cli {
+    use clap::SubCommand;
+    pub fn get_args() -> clap::ArgMatches<'static> {
+        clap::App::new(clap::crate_name!())
+            .version(clap::crate_version!())
+            .about(clap::crate_description!())
+            .setting(clap::AppSettings::DeriveDisplayOrder)
+            .arg(
+                clap::Arg::with_name("verbosity")
+                    .short("v")
+                    .multiple(true)
+                    .global(true)
+                    .help("Sets the level of verbosity"),
+            )
+            .subcommand(
+                SubCommand::with_name("test")
+                .arg(clap::Arg::with_name("query").long("query").takes_value(true))
+                .arg(clap::Arg::with_name("flag").long("flag")),
+            )
+            .get_matches()
+    }
 }
---
+++ src/srt_loader.rs
@@ -0,0 +1,83 @@
+use anyhow::{Context, Result};
+use std::collections::HashMap;
+use std::{io::Read, path};
+
+const ADSUBS: &str = "/home/scott/Dropbox/Development/ArrestedDevelopmentSubs";
+const FILE: &str = "Arrested Development - 1x03 - Bringing Up Buster.HDTV.en.srt";
+
+fn is_srt(p: &path::Path) -> bool {
+    let oext = p.extension();
+    oext.map(|ext| ext == "srt").unwrap_or(false)
+}
+
+fn rough_title(p: &path::Path) -> String {
+    let fname = p
+        .file_name()
+        .map(|oss| oss.to_string_lossy())
+        .unwrap_or("unknown".into());
+    let fname = fname.split('.').next().unwrap_or(fname.as_ref());
+    fname.to_string()
+}
+
+fn list_subs<P: AsRef<path::Path>>(root: P) -> Result<HashMap<String, String>> {
+    let root = root.as_ref();
+    let mut subs = HashMap::new();
+    for dir in walkdir::WalkDir::new(root)
+        .into_iter()
+        .filter(|de| de.as_ref().map(|de| is_srt(de.path())).unwrap_or(true))
+    {
+        let dir = dir?;
+        let name = rough_title(dir.path());
+        log::trace!("open subtitles {:?}", dir.path());
+
+        match read_file(dir.path()).and_then(|s| srt_to_script(s.as_ref())) {
+            Ok(s) => {
+                subs.insert(name, s);
+            }
+            Err(e) => log::error!("{:?}: {}", dir.path(), e),
+        }
+    }
+
+    Ok(subs)
+}
+
+fn read_file<P: AsRef<path::Path>>(tpath: P) -> Result<String> {
+    let tpath = tpath.as_ref();
+    let mut f = std::fs::File::open(tpath)?;
+    let mut v = Vec::new();
+    f.read_to_end(&mut v)?;
+    Ok(String::from_utf8(v)?)
+}
+
+// TODO actually parse SRT
+fn srt_to_script(s: &str) -> Result<String> {
+    let mut r = String::new();
+    let mut reset = 2;
+    for line in s.lines() {
+        let text = line.trim().trim_start_matches('-').trim();
+        if text.is_empty() {
+            reset = 2;
+            continue;
+        }
+        if reset > 0 {
+            reset -= 1;
+            continue;
+        }
+        r.push_str(" ");
+        r.push_str(text);
+    }
+    Ok(r)
+}
+
+fn parse(text: &str) -> Result<()> {
+    let s = srt::parser::parse_srt_from_slice(text.as_bytes())?;
+    for sub in s.subs {
+        log::debug!("{:#?}", sub);
+    }
+    Ok(())
+}
+
+
+pub fn parse_adsubs() -> Result<HashMap<String, String>> {
+    Ok(list_subs(ADSUBS)?)
+}
